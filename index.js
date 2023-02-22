de452b26eae5733934d8f65cde7d4682c3ebc135
Lobzikk <71785494+Lobzikk@users.noreply.github.com>
2023-02-22 21:29:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-<<<<<<< Updated upstream
-require("dotenv").config();
-console.log(`PID:${process.pid}`);//to kill the process if needed
-const mysql2=require("mysql2");
-const langs=require('./langs.js');
-const {Client, LocalAuth}=require("whatsapp-web.js");
-const qrcode=require("qrcode-terminal");
-const shortFl=process.argv[2];
-let phrases=langs.en;
-if (process.argv[3]="ru") {
-    phrases=langs.ru;
-}
-const connection=mysql2.createConnection({
-    host:process.env.HOST,
-    user:process.env.USER,
-    database:process.env.DATABASE,
-    password:process.env.PASSWORD
-});
-connection.connect(function(err){
-    if (err){
-        return console.error(`${phrases[0]} ${err.message}`)
-=======
 const {app,BrowserWindow}=require("electron");
 const path=require("path");
 const createWindow = () => {
@@ -38,7 +16,6 @@ const createWindow = () => {
   app.on('window-all-closed',()=>{
     if (process.platform!=="darwin") {
         app.quit()
->>>>>>> Stashed changes
     }
   });
   
